0c7a0e7a2b2617a23e0a1785700de759f368c109
Seungah Lee <cnu4530@snu.ac.kr>
2022-02-13 17:00:39 +0900

    [FEAT] 할 일 수정 ajax로 구현(같은 카테고리 내에서만 수정)

--- static/js/home/date_todo.js
+++ static/js/home/date_todo.js
@@ -201,6 +201,53 @@ const deleteHandleResponse = () => {
     }
 };
 
+//할 일 수정 ajax
+const reqEditTodo = new XMLHttpRequest();
+
+function editTodoBtn(event, select_date) {
+    todo_id = edit_btn.classList[3];
+    const form = new FormData(document.querySelector('#setToDoModal form'));
+    var form_data = serialize(form);
+
+    const url = `./${select_date}/${todo_id}/edit/`;
+    reqEditTodo.open("POST", url, true);
+    reqEditTodo.setRequestHeader(
+        "Content-Type",
+        "application/x-www-form-urlencoded",
+    );
+    reqEditTodo.send(JSON.stringify({
+        todo_id : todo_id,
+        form_data : form_data,
+    }));
+}
+
+reqEditTodo.onreadystatechange = () => {
+    if (reqEditTodo.readyState === XMLHttpRequest.DONE) {
+        editHandleResponse();
+    }
+};
+
+const editHandleResponse = () => {
+    if (reqEditTodo.status < 400) {
+
+        console.log(JSON.parse(reqEditTodo.response));
+        const {todo_id, content, priority_num} = JSON.parse(reqEditTodo.response);
+        const edit_todo_div = document.querySelector(`.todo-id-${todo_id}`);
+
+        const priority_icon = edit_todo_div.querySelector(`i.fa-fire`);
+        priority_icon.classList.replace('1', `${priority_num}`);
+        priority_icon.classList.replace('2', `${priority_num}`);
+        priority_icon.classList.replace('3', `${priority_num}`);
+
+        const edit_content = edit_todo_div.querySelector('p.todo-text');
+        edit_content.innerHTML = content;
+        console.log(edit_todo_div);
+    }
+};
+
+
+
+
 
 // Utils
 function serialize (data) {
