686d100397554caf570774f49a4535bfca2441ac
Seungah Lee <cnu4530@snu.ac.kr>
2022-02-21 01:23:32 +0900

    [FIX] django static 가져오기

--- config/settings.py
+++ config/settings.py
@@ -136,7 +136,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = '/static/.'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    '/var/www/static']
 
 
 # Default primary key field type
